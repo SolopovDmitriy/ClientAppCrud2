f8dc37b4dc13a04f076d037e76b39260d9171f3a
Dmitriy Solopov <div.solopov@gmail.com>
2021-08-01 16:58:19 +0300

    first commit

---
+++ ClientApp/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Query = new System.Windows.Forms.TextBox();
+            this.button_Execute = new System.Windows.Forms.Button();
+            this.dataGridView_Results = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_Info = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button_Exexute_set = new System.Windows.Forms.Button();
+            this.button_ExecUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_RowFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_SortFilter = new System.Windows.Forms.TextBox();
+            this.button_FilterExec = new System.Windows.Forms.Button();
+            this.button_SortExec = new System.Windows.Forms.Button();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonPaint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_Query
+            // 
+            this.textBox_Query.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Query.Location = new System.Drawing.Point(16, 16);
+            this.textBox_Query.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Query.Name = "textBox_Query";
+            this.textBox_Query.Size = new System.Drawing.Size(816, 36);
+            this.textBox_Query.TabIndex = 0;
+            // 
+            // button_Execute
+            // 
+            this.button_Execute.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Execute.Location = new System.Drawing.Point(843, 10);
+            this.button_Execute.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Execute.Name = "button_Execute";
+            this.button_Execute.Size = new System.Drawing.Size(217, 76);
+            this.button_Execute.TabIndex = 1;
+            this.button_Execute.Text = "Execute DataTable";
+            this.button_Execute.UseVisualStyleBackColor = true;
+            this.button_Execute.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // dataGridView_Results
+            // 
+            this.dataGridView_Results.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_Results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Results.Location = new System.Drawing.Point(16, 60);
+            this.dataGridView_Results.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView_Results.Name = "dataGridView_Results";
+            this.dataGridView_Results.RowHeadersWidth = 51;
+            this.dataGridView_Results.Size = new System.Drawing.Size(816, 576);
+            this.dataGridView_Results.TabIndex = 2;
+            this.dataGridView_Results.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Results_CellLeave);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_Info});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 680);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1067, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_Info
+            // 
+            this.toolStripStatusLabel_Info.Name = "toolStripStatusLabel_Info";
+            this.toolStripStatusLabel_Info.Size = new System.Drawing.Size(0, 16);
+            // 
+            // button_Exexute_set
+            // 
+            this.button_Exexute_set.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Exexute_set.Location = new System.Drawing.Point(843, 95);
+            this.button_Exexute_set.Margin = new System.Windows.Forms.Padding(4);
+            this.button_Exexute_set.Name = "button_Exexute_set";
+            this.button_Exexute_set.Size = new System.Drawing.Size(217, 71);
+            this.button_Exexute_set.TabIndex = 4;
+            this.button_Exexute_set.Text = "Execute DataSet";
+            this.button_Exexute_set.UseVisualStyleBackColor = true;
+            this.button_Exexute_set.Click += new System.EventHandler(this.button_Exexute_set_Click);
+            // 
+            // button_ExecUpdate
+            // 
+            this.button_ExecUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ExecUpdate.Location = new System.Drawing.Point(843, 192);
+            this.button_ExecUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.button_ExecUpdate.Name = "button_ExecUpdate";
+            this.button_ExecUpdate.Size = new System.Drawing.Size(217, 49);
+            this.button_ExecUpdate.TabIndex = 5;
+            this.button_ExecUpdate.Text = "Custom Update";
+            this.button_ExecUpdate.UseVisualStyleBackColor = true;
+            this.button_ExecUpdate.Click += new System.EventHandler(this.button_ExecUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(843, 251);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Фильтр по:";
+            // 
+            // textBox_RowFilter
+            // 
+            this.textBox_RowFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_RowFilter.Location = new System.Drawing.Point(843, 287);
+            this.textBox_RowFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_RowFilter.Name = "textBox_RowFilter";
+            this.textBox_RowFilter.Size = new System.Drawing.Size(216, 36);
+            this.textBox_RowFilter.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(841, 377);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Сортировака по:";
+            // 
+            // textBox_SortFilter
+            // 
+            this.textBox_SortFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_SortFilter.Location = new System.Drawing.Point(841, 412);
+            this.textBox_SortFilter.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_SortFilter.Name = "textBox_SortFilter";
+            this.textBox_SortFilter.Size = new System.Drawing.Size(216, 36);
+            this.textBox_SortFilter.TabIndex = 7;
+            // 
+            // button_FilterExec
+            // 
+            this.button_FilterExec.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_FilterExec.Location = new System.Drawing.Point(843, 332);
+            this.button_FilterExec.Margin = new System.Windows.Forms.Padding(4);
+            this.button_FilterExec.Name = "button_FilterExec";
+            this.button_FilterExec.Size = new System.Drawing.Size(217, 41);
+            this.button_FilterExec.TabIndex = 8;
+            this.button_FilterExec.Text = "Filter Execute";
+            this.button_FilterExec.UseVisualStyleBackColor = true;
+            this.button_FilterExec.Click += new System.EventHandler(this.button_FilterExec_Click);
+            // 
+            // button_SortExec
+            // 
+            this.button_SortExec.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_SortExec.Location = new System.Drawing.Point(841, 457);
+            this.button_SortExec.Margin = new System.Windows.Forms.Padding(4);
+            this.button_SortExec.Name = "button_SortExec";
+            this.button_SortExec.Size = new System.Drawing.Size(217, 41);
+            this.button_SortExec.TabIndex = 8;
+            this.button_SortExec.Text = "Sorted Execute";
+            this.button_SortExec.UseVisualStyleBackColor = true;
+            this.button_SortExec.Click += new System.EventHandler(this.button_SortExec_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpenFile.Location = new System.Drawing.Point(841, 522);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(218, 39);
+            this.buttonOpenFile.TabIndex = 9;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonPaint
+            // 
+            this.buttonPaint.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPaint.Location = new System.Drawing.Point(843, 589);
+            this.buttonPaint.Name = "buttonPaint";
+            this.buttonPaint.Size = new System.Drawing.Size(214, 47);
+            this.buttonPaint.TabIndex = 10;
+            this.buttonPaint.Text = "Paint";
+            this.buttonPaint.UseVisualStyleBackColor = true;
+            this.buttonPaint.Click += new System.EventHandler(this.buttonPaint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 702);
+            this.Controls.Add(this.buttonPaint);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.button_SortExec);
+            this.Controls.Add(this.button_FilterExec);
+            this.Controls.Add(this.textBox_SortFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_RowFilter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_ExecUpdate);
+            this.Controls.Add(this.button_Exexute_set);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView_Results);
+            this.Controls.Add(this.button_Execute);
+            this.Controls.Add(this.textBox_Query);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Query;
+        private System.Windows.Forms.Button button_Execute;
+        private System.Windows.Forms.DataGridView dataGridView_Results;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Info;
+        private System.Windows.Forms.Button button_Exexute_set;
+        private System.Windows.Forms.Button button_ExecUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_RowFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_SortFilter;
+        private System.Windows.Forms.Button button_FilterExec;
+        private System.Windows.Forms.Button button_SortExec;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonPaint;
+    }
+}
+
---
+++ ClientApp/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ClientApp
+{
+    public partial class Form1 : Form
+    {
+        private string _dp;
+        private string _connectionString;
+
+        private DbProviderFactory _dbProviderFactory;
+        private DbConnection _dbConn;
+        private DataTable _dataTable;
+        private DbDataAdapter _dataAdapter;
+        private DataSet _dataSet;
+
+        string filePath;
+
+        //[System.ComponentModel.Browsable(false)]
+        public override bool AutoSize { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            _dp = ConfigurationManager.AppSettings["MSSQLProvider"];
+            _connectionString = ConfigurationManager.ConnectionStrings["MSSQLProvider"].ConnectionString;
+            _dbProviderFactory = DbProviderFactories.GetFactory(_dp);
+            _dbConn = _dbProviderFactory.CreateConnection();
+            _dbConn.ConnectionString = _connectionString;
+            _dataAdapter = _dbProviderFactory.CreateDataAdapter();
+            _dataSet = new DataSet();
+            textBox_Query.Text = "select * from users_info";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //_dataTable = new DataTable(); //пустая таблица
+            ///*Определяю столбцы*/
+            //_dataTable.Columns.Add("Id");
+            //_dataTable.Columns.Add("Login");
+            //_dataTable.Columns.Add("Email");
+            //_dataTable.Columns.Add("Password");
+
+            ///*создаю строку данных*/
+            //DataRow dataRow = _dataTable.NewRow();
+            //dataRow[0] = 1;
+            //dataRow[1] = "Test user";
+            //dataRow[2] = "testemail.om";
+            //dataRow[3] = "qwerty";
+
+            //_dataTable.Rows.Add(dataRow);
+            ///*привязываю DataTable к DataGridView*/
+            //dataGridView_Results.DataSource = _dataTable;
+        }
+
+        private void button_Execute_Click(object sender, EventArgs e)
+        {
+            DbDataReader resultReader = null;
+            try
+            {
+                if (textBox_Query.Text.Length >= 5)
+                {
+                    _dataTable = new DataTable();
+
+                    DbCommand dbSelectAllUsersCommand = _dbProviderFactory.CreateCommand();
+                    dbSelectAllUsersCommand.Connection = _dbConn;
+                    dbSelectAllUsersCommand.CommandText = textBox_Query.Text;
+                    _dbConn.Open();
+                    resultReader = dbSelectAllUsersCommand.ExecuteReader();
+                    //toolStripStatusLabel_Info.Text = resultReader.
+                    int line = 0;
+                    do
+                    {
+                        while (resultReader.Read())
+                        {
+                            if (line == 0)
+                            {
+                                for (int i = 0; i < resultReader.FieldCount; i++)
+                                {
+                                    _dataTable.Columns.Add(resultReader.GetName(i));
+                                }
+                                line++;
+                            }
+                                DataRow currentRow = _dataTable.NewRow();
+                                for (int i = 0; i < resultReader.FieldCount; i++)
+                                {
+                                    currentRow[i] = resultReader[i];
+                                }
+                                _dataTable.Rows.Add(currentRow);
+                        }
+                    } while (resultReader.NextResult());
+                    dataGridView_Results.DataSource = _dataTable;
+                }
+                else
+                {
+                    toolStripStatusLabel_Info.ForeColor = Color.Red;
+                    toolStripStatusLabel_Info.Text = "Тело  запроса не межет быть пустым";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _dbConn.Close();
+                if(resultReader != null) resultReader.Close();
+            }
+        }
+
+        private void button_Exexute_set_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox_Query.Text.Length >= 5)
+                {
+                    //_dataAdapter.SelectCommand
+                    //_dataAdapter.InsertCommand
+                    //_dataAdapter.UpdateCommand
+                    //_dataAdapter.DeleteCommand
+
+
+                    //_dataAdapter.Fill() //select
+                    //_dataAdapter.Update(); //insert-delete-update
+
+                    _dataSet.Clear();
+                    DbCommand command = _dbProviderFactory.CreateCommand();
+                    command.Connection = _dbConn;
+                    command.CommandText = textBox_Query.Text;
+                    _dataAdapter.SelectCommand = command;
+                    _dataAdapter.Fill(_dataSet);
+                    dataGridView_Results.DataSource = _dataSet.Tables[0];
+
+                    Debug.WriteLine(_dataAdapter.UpdateCommand);
+                    Debug.WriteLine(_dataAdapter.InsertCommand);
+                    Debug.WriteLine(_dataAdapter.DeleteCommand);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _dbConn.Close();
+            }
+        }
+
+        private void button_ExecUpdate_Click(object sender, EventArgs e)
+        {
+            //Пользовательская логика синхронизации GridView <=> dataAdapter <=> DB
+            //_dataAdapter.SelectCommand
+            //_dataAdapter.InsertCommand
+            //_dataAdapter.UpdateCommand
+            //_dataAdapter.DeleteCommand
+            DbCommand dbSelectCommand = _dbProviderFactory.CreateCommand();
+            dbSelectCommand.Connection = _dbConn;
+            dbSelectCommand.CommandText = "select * from users_info";
+            _dataAdapter.SelectCommand = dbSelectCommand;
+
+            _dataAdapter.Fill(_dataSet);
+
+            /*DbCommandBuilder dbCommandBuilder = _dbProviderFactory.CreateCommandBuilder();
+            dbCommandBuilder.DataAdapter = _dataAdapter;
+            dbCommandBuilder.GetDeleteCommand();
+            dbCommandBuilder.GetInsertCommand();
+            dbCommandBuilder.GetUpdateCommand();*/
+
+
+            /*Пользовательская логика - UpdateCommand -----------------------------------------------------start*/
+            //DbCommand dbCommandUpdate = _dbProviderFactory.CreateCommand();
+            //dbCommandUpdate.Connection = _dbConn;
+            //dbCommandUpdate.CommandText = "UPDATE users SET password = @pPassword WHERE Id = @iId";
+
+            //DbParameter dbPasswordParameter = dbCommandUpdate.CreateParameter();
+            //dbPasswordParameter.DbType = DbType.String;
+            //dbPasswordParameter.ParameterName = "@pPassword";
+            //dbPasswordParameter.SourceVersion = DataRowVersion.Current;
+            //dbPasswordParameter.SourceColumn = "password";
+            //dbCommandUpdate.Parameters.Add(dbPasswordParameter);
+
+            //DbParameter dbIdParameter = dbCommandUpdate.CreateParameter();
+            //dbIdParameter.DbType = DbType.Int32;
+            //dbIdParameter.ParameterName = "@iId";
+            //dbIdParameter.SourceColumn = "Id";
+            //dbIdParameter.SourceVersion = DataRowVersion.Original;
+            //dbCommandUpdate.Parameters.Add(dbIdParameter);
+
+            //_dataAdapter.UpdateCommand = dbCommandUpdate;
+            /*Пользовательская логика - UpdateCommand ------------------------------------------------------------end*/
+
+
+
+
+
+
+            /*Пользовательская логика - InsertCommand ----------start*/
+            // TODO change size
+            byte[] file;
+            //filePath = @"D:\Новая папка (4)\Screenshots\Снимок экрана (1).png";
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    file = reader.ReadBytes((int)stream.Length);
+                    DbCommand dbCommandInsert = _dbProviderFactory.CreateCommand();
+                    dbCommandInsert.Connection = _dbConn;
+                    dbCommandInsert.CommandText = "insert into users_info (user_id, email, avatar) Values(@user_id2, @email2, @avatar2)";
+
+                    DbParameter user_idParam = dbCommandInsert.CreateParameter();
+                    user_idParam.DbType = DbType.Int32;
+                    user_idParam.ParameterName = "@user_id2";
+                    user_idParam.SourceVersion = DataRowVersion.Current;
+                    user_idParam.SourceColumn = "user_id";
+                    dbCommandInsert.Parameters.Add(user_idParam);
+
+                    DbParameter emailParam = dbCommandInsert.CreateParameter();
+                    emailParam.DbType = DbType.String;
+                    emailParam.ParameterName = "@email2";
+                    emailParam.SourceColumn = "email";
+                    emailParam.SourceVersion = DataRowVersion.Original;
+                    dbCommandInsert.Parameters.Add(emailParam);
+
+                    
+                    DbParameter avatarParam = dbCommandInsert.CreateParameter();
+                    avatarParam.DbType = DbType.Binary;
+                    avatarParam.ParameterName = "@avatar2";
+                    avatarParam.Value = file;
+                    avatarParam.Size = file.Length;
+                    Debug.WriteLine(filePath);
+                    dbCommandInsert.Parameters.Add(avatarParam);
+
+                    //dbCommandInsert.Parameters.Add("@File", SqlDbType.VarBinary, file.Length).Value = file;
+                   
+                    _dataAdapter.InsertCommand = dbCommandInsert;
+                }
+            }
+            /*Пользовательская логика - InsertCommand ----------end*/
+
+
+
+
+
+            _dataAdapter.Update(_dataSet);
+
+            _dataSet.Clear();
+            _dataAdapter.Fill(_dataSet);
+            dataGridView_Results.DataSource = _dataSet.Tables[0];
+        }
+
+        private void dataGridView_Results_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button_FilterExec_Click(object sender, EventArgs e)
+        {
+            if(textBox_RowFilter.Text.Length >= 5)
+            {
+                DataViewManager dataViewManager = new DataViewManager(_dataSet);
+                dataViewManager.DataViewSettings[0].RowFilter = textBox_RowFilter.Text;
+
+                DataView dataViewFiltered = dataViewManager.CreateDataView(_dataSet.Tables[0]);
+                dataGridView_Results.DataSource = dataViewFiltered;
+            }
+        }
+
+        private void button_SortExec_Click(object sender, EventArgs e)
+        {
+
+            if (textBox_SortFilter.Text.Length >= 3)
+            { 
+                DataViewManager dataViewManager = new DataViewManager(_dataSet);
+                dataViewManager.DataViewSettings[0].Sort = textBox_SortFilter.Text;
+
+                DataView dataViewSorted = dataViewManager.CreateDataView(_dataSet.Tables[0]);
+                dataGridView_Results.DataSource = dataViewSorted;
+            }
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {               
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {                    
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            this.textBox_RowFilter.AutoSize = false;
+            this.textBox_RowFilter.Text = "Hello world!";
+            this.textBox_RowFilter.Font = new System.Drawing.Font("ArialBlack", 8, FontStyle.Regular);
+
+
+        }
+
+        private void buttonPaint_Click(object sender, EventArgs e)
+        {
+            Process.Start("mspaint.exe");
+        }
+    }
+}
